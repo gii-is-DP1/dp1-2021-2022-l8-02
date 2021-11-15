c778be656cfa44402dfb85cef06b39ade2b747dc
Migue <miggomgom1@alum.us.es>
2021-11-15 14:20:48 +0100

    Cambiados los nombres de 2 archivos. Pet Clinic --->> EOL

--- src/main/java/org/springframework/samples/petclinic/EndOfLineApplication.java
+++ src/main/java/org/springframework/samples/petclinic/EndOfLineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class PetclinicApplication {
+public class EndOfLineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+		SpringApplication.run(EndOfLineApplication.class, args);
 	}
 
-}
+} 
