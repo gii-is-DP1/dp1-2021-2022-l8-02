d7e655c50a8b4fd8a1d9feff8e7fe24320ddef80
Francisco Javier Vila Pavon <kikovilapavon@gmail.com>
2021-11-26 00:02:28 +0100

    Test de creacion de juego para probar las estadisticas conseguido

--- src/test/java/org/springframework/samples/petclinic/endofline/GameStatisticsTest/GameStatisticsTest.java
+++ src/test/java/org/springframework/samples/petclinic/endofline/GameStatisticsTest/GameStatisticsTest.java
@@ -3,32 +3,22 @@ package org.springframework.samples.petclinic.endofline.GameStatisticsTest;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.samples.endofline.board.Board;
-import org.springframework.samples.endofline.board.Tile;
-import org.springframework.samples.endofline.board.TileState;
-import org.springframework.samples.endofline.card.Card;
-import org.springframework.samples.endofline.card.CardColor;
-import org.springframework.samples.endofline.card.CardType;
-import org.springframework.samples.endofline.card.Direction;
+import org.springframework.samples.endofline.card.CardService;
 import org.springframework.samples.endofline.game.Game;
 import org.springframework.samples.endofline.game.GameMode;
 import org.springframework.samples.endofline.game.GameService;
-import org.springframework.samples.endofline.game.GameState;
+import org.springframework.samples.endofline.game.exceptions.DuplicatedGameNameException;
 import org.springframework.samples.endofline.usuario.Authorities;
 import org.springframework.samples.endofline.usuario.Usuario;
+import org.springframework.samples.endofline.usuario.UsuarioService;
 import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
@@ -37,94 +27,61 @@ public class GameStatisticsTest {
 
     @Autowired
     protected GameService gameService;
+    @Autowired
+    protected UsuarioService userService;
+    @Autowired
+    protected CardService cardService;
+    @Autowired
+    protected 
+    
 
     @BeforeEach
     void newGame() {
-        Usuario usuario = new Usuario();
-        Game gameG = new Game();
-        Board board = new Board();
-        Tile tile1 = new Tile();
-        Tile tile2 = new Tile();
-        Tile tile3 = new Tile();
-        Tile tile4 = new Tile();
-        Card card = new Card();
-        CardType cardType = new CardType();
-        List<Tile> listTile = new ArrayList<>();
-        List<Usuario> listUserName = new ArrayList<>();
-        Authorities authorities = new Authorities();
+
+        Game game = new Game();
+        game.setName("Juego Prueba Estadisticas");
+        game.setGameMode(GameMode.SOLITAIRE);
+        game.setHidden(false);
+        Usuario user = new Usuario();
+        Authorities aut = new Authorities();
         Set<Authorities> setAut = new HashSet<>();
-        List<Direction> direction = new ArrayList<>();
 
-        authorities.setId(1);
-        authorities.setUsuario(usuario);
-        authorities.setAuthority("Admin");
+        //Usuario
+        user.setUsername("player");
+        user.setPassword("pass");
+        user.setEmail("player@endofline.com");
+        aut.setAuthority("jugador");
+        setAut.add(aut);
+        user.setAuthorities(setAut);
+        userService.save(user);
 
-        usuario.setEmail("newuser@gmail.com");
-        usuario.setAuthorities(setAut);
+        try{
 
-        usuario.setPassword("123ttf");
-        usuario.setUsername("player");
+            gameService.createGame(game);
+            gameService.joinGame(game, user);
+            gameService.startGame(game);
 
-        direction.add(Direction.NORTH);
-        direction.add(Direction.WEST);
-        cardType.setId(1);
-        cardType.setDirections(direction);
-        cardType.setDirections(direction);
-        cardType.setIniciative(5);
-        cardType.setName("photoCard.jpg");
+        }catch(DuplicatedGameNameException e){
 
-        card.setId(1);
-        card.setCardType(cardType);
-        card.setColor(CardColor.RED);
+            System.out.println("Ya existe la partida");
 
-        tile1.setId(1);
-        tile1.setCard(card);
-        tile1.setTileState(TileState.TAKEN);
-        tile1.setX(0);
-        tile1.setY(0);
-        tile1.setBoard(board);
-
-        tile2.setId(2);
-        tile2.setTileState(TileState.FREE);
-        tile2.setX(1);
-        tile2.setY(0);
-        tile2.setBoard(board);
-
-        tile3.setId(3);
-        tile3.setTileState(TileState.FREE);
-        tile3.setX(0);
-        tile3.setY(1);
-        tile3.setBoard(board);
-
-        tile4.setId(4);
-        tile4.setTileState(TileState.FREE);
-        tile4.setX(1);
-        tile4.setY(1);
-        tile4.setBoard(board);
-
-        listTile.add(tile1);
-        listTile.add(tile2);
-        listTile.add(tile3);
-        listTile.add(tile4);
-
-        board.setId(1);
-        board.setTiles(listTile);
-        board.setGame(gameG);
-        gameG.setName("Game1");
-        gameG.setId(1);
-        gameG.setHidden(false);
-        listUserName.add(usuario);
-        gameG.setPlayers(listUserName);
-        gameG.setGameMode(GameMode.SOLITAIRE);
-        gameG.setGameState(GameState.PLAYING);
-        gameG.setBoard(board);
-
-        try {
-            gameService.createGame(gameG);
-        } catch (Exception e) {
-            System.out.println("Ya existe este juego");
         }
+    }
 
+    @Test
+    void shouldExistGame(){
+
+        Game game1 = gameService.getGameByPlayer(userService.findByUsername("player").get());
+        assertNotNull(game1);
+
+    }
+
+    @Test
+    void shouldExistStatistics(){
+
+        Game game1 = gameService.getGameByPlayer(userService.findByUsername("player").get());
+
+        assertNotNull(game1);
     }
 
 }
